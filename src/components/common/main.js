cc1c1c05dc21b0fa18a02e376beb6533d92b9316
PrateekKumarSingh <calledasprateek@outlook.com>
2023-08-26 15:52:23 +0530

    new commit

--- src/components/common/main.js
+++ src/components/common/main.js
@@ -5,35 +5,84 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { MovieData } from '../constants/moviedata/moviedata';
 
 export default function ImgMediaCard() {
+  const cardContainerStyle = {
+    backgroundImage: 'url("https://images.freecreatives.com/wp-content/uploads/2015/03/Huge-Backgrounds-17.jpg")', // Replace with your background image URL
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
   return (
-    <>
-    
-    {MovieData}
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        img
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          title
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    </>
-    
+    <div style={cardContainerStyle}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          img src="https://tse1.mm.bing.net/th?id=OIP.4bLBoKQnJMelU1pDOFMARwHaEK&pid=Api&P=0&h=220.jpg" 
+          padding="20px"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            title
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          img src="https://tse1.mm.bing.net/th?id=OIP.4bLBoKQnJMelU1pDOFMARwHaEK&pid=Api&P=0&h=220.jpg" 
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            title
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          img src="https://tse1.mm.bing.net/th?id=OIP.4bLBoKQnJMelU1pDOFMARwHaEK&pid=Api&P=0&h=220.jpg" 
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            title
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
--- src/components/style/homepage.js
+++ src/components/style/homepage.js
@@ -5,33 +5,12 @@ import ImgMediaCard from "../common/main";
 import axios from "axios";
 function Homepage (){
 
-    // const options = {
-    //     method: 'GET',
-    //     url: 'https://imdb-top-100-movies.p.rapidapi.com/',
-    //     headers: {
-    //       'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-    //       'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-    //     }
-    //   };
-      
-    //   async function getMoviesData() {
-    //       try{
-    //       const response = await axios.request(options);
-    //       console.log(response.data);
-    //   } catch (error) {
-    //       console.error(error);
-    //   }}
-    //   useEffect(() => 
-    //   {
-    //       getMoviesData();
-    //   }, []);
-
-
 
     return<>
     <div>
     <Header/>
     <h1>Welcome to MOVIEBAZAAR</h1>
+   
     <ImgMediaCard/>
     <Footer/>
     </div>
